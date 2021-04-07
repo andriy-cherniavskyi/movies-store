0e1aee6f12201b8af6b8e1e04ce08d1b8f5bd389
a.cherniavskyi <andrii.cherniavskyi@eliftech.com>
2021-04-07 22:42:55 +0300

    init project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "23f17c9c6ba936f26ee1";
+/******/ 	var hotCurrentHash = "32892808e1b2128f2a1b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
